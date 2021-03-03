f5d1f51004dbacb338612af0dc0fc037f545aa61
SurhayEnes <76548885+SurhayEnes@users.noreply.github.com>
2021-03-03 20:21:32 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -17,10 +17,10 @@ exports.run = (client, message) => {
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ["yardım"],
+    aliases: ["help"],
     permLevel: 0
   };
   
   exports.help = {
-    name: "valorant"
+    name: "yardım"
   };
